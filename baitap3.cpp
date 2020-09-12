85a324832e8e59d4ca096e27ead9d1a1e8ee05b7
thieugia882014 <70931357+thieugia882014@users.noreply.github.com>
2020-09-12 14:04:30 +0700

    Add files via upload

---
+++ baitap3.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int a,b;
+	printf("nhap a=");
+	scanf("%d",&a);
+	printf("nhap b=");
+	scanf("%d",&b);
+	
+	if(a>=b){
+		int c=a/b;
+		printf("%d ket qua",c);
+		
+		}else{
+		int c=a*b;
+	    printf("%d ket qua",c);
+	    
+			
+		}
+	
+	
+	
+	
+	
+	}
+	

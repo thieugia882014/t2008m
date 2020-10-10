426b604274f79824d3c8b332684e2cc95266c69a
thieugia882014 <minhtuanmk1@gmail.com>
2020-10-10 23:39:43 +0700

    code len githud

--- lab3-2.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <cmath>
-int main(){
-	float T;
-	printf("4000$ gui ngan hang voi lai suat la 8% 1 nam\n");
-	T=4000*1.08*1.08*1.08*1.08;
-	printf("So tien nhan duoc sau 4 nam la: %0.2f",T);
-}
